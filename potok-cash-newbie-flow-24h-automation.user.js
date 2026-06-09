6b1034b6139012ffcd5894409eec93e14a210c97
valpolik <92299383+valpolik@users.noreply.github.com>
2026-06-09 11:30:58 +0300

    Update version number in user script metadata

--- potok-cash-newbie-flow-24h-automation.user.js
+++ potok-cash-newbie-flow-24h-automation.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Potok Cash Bonus Keeper [newbie]
 // @namespace    https://potok.cash/cabinet
-// @version      20260525054846
+// @version      20260609082659
 // @description  Automation for pressing bonus button each 24 hours
 // @author       https://github.com/valpolik/potok-cash-newbie-flow-24h-automation
 // @match        https://potok.cash/cabinet
